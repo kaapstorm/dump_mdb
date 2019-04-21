99ae5c4e2b46a2cbf10ed7b24096e5d739c0efa2
Norman Hooper <norman@kaapstorm.com>
2019-04-22 01:28:30 +0200

    Dump table

--- dump_mdb/app.py
+++ dump_mdb/app.py
@@ -1,11 +1,44 @@
-from flask import Flask, Response
+import os
+from contextlib import closing
+from subprocess import check_output
+from tempfile import NamedTemporaryFile
+
+from flask import Flask, render_template, Response
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileRequired
+from wtforms import StringField, FileField
+from wtforms.validators import DataRequired
+
+MDB_EXPORT_CMD = '/usr/bin/mdb-export'
 
 
 app = Flask(__name__)
+app.config.from_mapping(WTF_CSRF_ENABLED=False)
 
-@app.route("/")
+
+class MdbTableForm(FlaskForm):
+    mdb_file = FileField('MDB file', validators=[FileRequired()])
+    table_name = StringField('Table name', validators=[DataRequired()])
+
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return Response("It works!"), 200
+    form = MdbTableForm()
+    if form.validate_on_submit():
+        return Response(
+            dump_table(form.mdb_file, form.table_name), mimetype='text/csv'
+        ), 200
+    return render_template('mdb_table_form.html', form=form)
+
+
+def dump_table(mdb_file, table_name):
+    table_name = str(table_name.data)
+    with closing(NamedTemporaryFile()) as tmp_file:
+        mdb_file.data.save(tmp_file)
+        output = check_output(
+            (MDB_EXPORT_CMD, tmp_file.name, table_name)
+        )
+    return output
 
 
 if __name__ == "__main__":
