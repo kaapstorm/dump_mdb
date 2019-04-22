b530add3ceab0cde3150dae3c20bc786c1086ace
Norman Hooper <norman@kaapstorm.com>
2019-04-22 10:51:15 +0200

    Require basic auth

--- dump_mdb/app.py
+++ dump_mdb/app.py
@@ -1,15 +1,18 @@
 import os
 from contextlib import closing
+from functools import wraps
 from subprocess import check_output, CalledProcessError
 from tempfile import NamedTemporaryFile
 
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired
 from wtforms import StringField, FileField
 from wtforms.validators import DataRequired
 
 MDB_EXPORT_CMD = '/usr/bin/mdb-export'
+BASIC_USERNAME = os.environ['BASIC_USERNAME']
+BASIC_PASSWORD = os.environ['BASIC_PASSWORD']
 
 
 app = Flask(__name__)
@@ -21,7 +24,29 @@ class MdbTableForm(FlaskForm):
     table_name = StringField('Table name', validators=[DataRequired()])
 
 
+def check_auth(username, password):
+    return username == BASIC_USERNAME and password == BASIC_PASSWORD
+
+
+def unauthorized():
+    return Response(
+        'Basic authentication failed', 401,
+        headers={'WWW-Authenticate': 'Basic realm="Login required"'}
+    )
+
+
+def requires_auth(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return unauthorized()
+        return func(*args, **kwargs)
+    return decorated
+
+
 @app.route("/", methods=['GET', 'POST'])
+@requires_auth
 def index():
     form = MdbTableForm()
     if form.validate_on_submit():
@@ -31,7 +56,7 @@ def index():
         except CalledProcessError:
             response = ("That didn't work. "
                         "Please check the file is valid and the table exists")
-            mimetype = 'text/plain'
+            mimetype = None
         return Response(response, 200, mimetype=mimetype)
     return render_template('mdb_table_form.html', form=form)
 
